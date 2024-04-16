6089ee918f5de420b244491ae9f969defe6b5a6f
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2024-04-15 23:34:56 -0400

    Encode the bigram count in a 2D array

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from textwrap import dedent
 from typing import List, Dict, Tuple
 import torch
+import matplotlib.pyplot as plt
 
 DATASET_PATH = './datasets/names.txt'
+START_SYMBOL = '<S>'
+END_SYMBOL = '<E>'
 
 def main():
   print(dedent(
@@ -16,20 +19,40 @@ def main():
   # e1_exploring_dataset()
   # e2_understanding_bigram()
   # e3_playing_with_tensors()
+  e4_understanding_bigram_using_2d_array()
 
 def e4_understanding_bigram_using_2d_array():
   """
   Convert the dictionary representation of the bigram into a 2D array
   """
-  names = read_names()
-  letters_count = get_letters_count(names)
-  extra_letters_count = 2 # Extra characters
+  names = read_names(DATASET_PATH)
+  letters = get_unique_letters_list(names)
+  stoi = encode_characters(letters)
+  N = len(stoi) # Number of unique characters
 
-  N = letters_count + extra_letters_count
+  # Initialize bigram count matrix
+  bigram_count = torch.zeros((N, N), dtype=torch.int32)
 
-  M = torch.zeros((N, N), dtype=torch.int32)
-  print(N)
+  # Count bigrams using the matrix
+  for name in names:
+    chars = [START_SYMBOL] + list(name) + [END_SYMBOL]
 
+    for char1, char2 in zip(chars, chars[1:]):
+      index_char1 = stoi[char1]
+      index_char2 = stoi[char2]
+
+      bigram_count[index_char1, index_char2] += 1
+
+  # Visualize the results
+  plt.imshow(bigram_count)
+  plt.show() # Keep the window open
+
+def encode_characters(characters: List[str]) -> Dict[str, int]:
+  stoi = {character: index for index, character in enumerate(characters)}
+  stoi[START_SYMBOL] = len(characters)
+  stoi[END_SYMBOL] = len(characters) + 1
+
+  return stoi
 
 def e3_playing_with_tensors():
   A = torch.zeros((5, 5), dtype=torch.int32)
@@ -78,17 +101,10 @@ def e1_exploring_dataset():
   print('Quantity of words: \t', len(names))
   print('Word with less characters: \t', min(len(n) for n in names))
   print('Word with the most characters: \t', max(len(n) for n in names))
-  print('Unique characters count: \t', get_letters_count(names))
+  print('Unique characters count: \t', len(get_unique_letters_list(names)))
 
-def get_letters_count(names: List[str]) -> int:
-  letters_count = {}
-
-  for name in names:
-    for letter in name:
-      letters_count[letter] = letters_count.get(letter, 0) + 1
-    
-  return len(letters_count)
-
+def get_unique_letters_list(names: List[str]) -> List[str]:
+  return sorted(list(set(''.join(names))))
 
 
 def read_names(file_path: str) -> List[str]:
