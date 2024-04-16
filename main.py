f51dda87a0fe56adfefc03e4f35a4873135cf0d8
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2024-04-15 22:53:51 -0400

    Play with torch tensors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textwrap import dedent
 from typing import List, Dict, Tuple
+import torch
 
 DATASET_PATH = './datasets/names.txt'
 
@@ -13,8 +14,32 @@ def main():
   ))
 
   # e1_exploring_dataset()
-  e2_understanding_bigram()
+  # e2_understanding_bigram()
+  # e3_playing_with_tensors()
 
+def e4_understanding_bigram_using_2d_array():
+  """
+  Convert the dictionary representation of the bigram into a 2D array
+  """
+  names = read_names()
+  letters_count = get_letters_count(names)
+  extra_letters_count = 2 # Extra characters
+
+  N = letters_count + extra_letters_count
+
+  M = torch.zeros((N, N), dtype=torch.int32)
+  print(N)
+
+
+def e3_playing_with_tensors():
+  A = torch.zeros((5, 5), dtype=torch.int32)
+
+  for i in range(5):
+    for j in range(5):
+      if i == j:
+        A[i, j] = 1
+
+  print(A)
 
 def e2_understanding_bigram():
   """
@@ -53,6 +78,17 @@ def e1_exploring_dataset():
   print('Quantity of words: \t', len(names))
   print('Word with less characters: \t', min(len(n) for n in names))
   print('Word with the most characters: \t', max(len(n) for n in names))
+  print('Unique characters count: \t', get_letters_count(names))
+
+def get_letters_count(names: List[str]) -> int:
+  letters_count = {}
+
+  for name in names:
+    for letter in name:
+      letters_count[letter] = letters_count.get(letter, 0) + 1
+    
+  return len(letters_count)
+
 
 
 def read_names(file_path: str) -> List[str]:
